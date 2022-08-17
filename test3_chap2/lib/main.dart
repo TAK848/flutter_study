7ed8724cb17d163f2e25d750134de723808a2764
TAK848 <pairs_char0j@icloud.com>
2022-08-17 20:47:11 +0900

    chap2-06 page遷移 Navigator

--- test3_chap2/lib/main.dart
+++ test3_chap2/lib/main.dart
@@ -1,29 +1,18 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+
+import 'test_page1.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,110 +22,14 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  late AnimationController _animationControler;
-  late Animation _animation;
-
-  _play() async {
-    setState(() {
-      _animationControler.forward();
-    });
-  }
-
-  _stop() async {
-    setState(() {
-      _animationControler.stop();
-    });
-  }
-
-  _reverse() async {
-    setState(() {
-      _animationControler.reverse();
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _animationControler = AnimationController(
-      vsync: this,
-      duration: const Duration(
-        seconds: 1,
-      ),
-    );
-    _animation = _animationControler.drive(
-      Tween(
-        begin: 0.0,
-        end: 2.0 * pi,
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _animationControler.dispose();
-    super.dispose();
-  }
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: AnimatedBuilder(
-          animation: _animation,
-          builder: (context, _) {
-            return Transform.rotate(
-              angle: _animation.value,
-              child: const Icon(
-                Icons.cached,
-                size: 100,
-              ),
-            );
-          },
-        ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FloatingActionButton(
-            onPressed: _play,
-            child: const Icon(
-              Icons.arrow_forward,
-            ),
-          ),
-          FloatingActionButton(
-            onPressed: _stop,
-            child: const Icon(
-              Icons.pause,
-            ),
-          ),
-          FloatingActionButton(
-            onPressed: _reverse,
-            child: const Icon(
-              Icons.arrow_back,
-            ),
-          ),
-        ],
-      ),
-    );
+    return const Scaffold(body: TestPage1());
   }
 }
