59dc14528242b0600570fd9b8378d05e52258b22
TAK848 <pairs_char0j@icloud.com>
2022-09-04 10:39:31 +0900

    autoDispose無し

--- test_riverpod1/lib/main.dart
+++ test_riverpod1/lib/main.dart
@@ -20,26 +20,78 @@ class MyApp extends StatelessWidget {
   }
 }
 
-final strProvider = Provider.family<String, String>((ref, name) {
-  return 'Hello $name';
-});
-
 class RiverpodSample extends ConsumerWidget {
   const RiverpodSample({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final text = ref.watch(strProvider('Flutter'));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Riverpod Sample'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'First Page',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 50),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const NextPage()));
+                },
+                child: const Text(
+                  'Transition to the next page',
+                  style: TextStyle(fontSize: 24),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+final stateProvider = StateProvider((ref) {
+  return 0;
+});
+
+class NextPage extends ConsumerWidget {
+  const NextPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final countStateController = ref.read(stateProvider.notifier);
+    final count = ref.watch(stateProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Riverpod Sample'),
       ),
       body: Center(
-        child: Text(
-          text,
-          style: const TextStyle(fontSize: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Next Page',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 50),
+            Text(
+              count.toString(),
+              style: const TextStyle(fontSize: 24),
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          countStateController.state++;
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
