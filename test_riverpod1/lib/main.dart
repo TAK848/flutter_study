98d098623ab80471b884bbfa82fdf892dae931f4
TAK848 <pairs_char0j@icloud.com>
2022-09-04 10:36:57 +0900

    Provider.family<>((){})

--- test_riverpod1/lib/main.dart
+++ test_riverpod1/lib/main.dart
@@ -20,25 +20,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class User {
-  User(this.name, this.age);
-  final String name;
-  final int age;
-}
-
-class UserStateNotifier extends StateNotifier<User> {
-  UserStateNotifier() : super(User("", 20));
-
-  void setName(String newName) => state = User(newName, state.age);
-  void setAge(int newAge) => state = User(state.name, newAge);
-
-  get age => state.age;
-  get name => state.name;
-}
-
-final stateNotifierProvider =
-    StateNotifierProvider<UserStateNotifier, User>((ref) {
-  return UserStateNotifier();
+final strProvider = Provider.family<String, String>((ref, name) {
+  return 'Hello $name';
 });
 
 class RiverpodSample extends ConsumerWidget {
@@ -46,63 +29,17 @@ class RiverpodSample extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final userStateController = ref.read(stateNotifierProvider.notifier);
-    final user = ref.watch(stateNotifierProvider.select((value) => value.name));
+    final text = ref.watch(strProvider('Flutter'));
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Riverpod Sample'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Center(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  const Text(
-                    "Age",
-                    style: TextStyle(fontSize: 24),
-                  ),
-                  const SizedBox(width: 20),
-                  Text(
-                    userStateController.age.toString(),
-                    style: const TextStyle(fontSize: 24),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  const Text(
-                    "Name",
-                    style: TextStyle(fontSize: 24),
-                  ),
-                  const SizedBox(width: 20),
-                  Text(
-                    user,
-                    style: const TextStyle(fontSize: 24),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                decoration: const InputDecoration(
-                  hintText: '名前を入力してください',
-                ),
-                onChanged: (text) {
-                  userStateController.setName(text);
-                },
-              ),
-            ],
-          ),
+      body: Center(
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 24),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          userStateController.setAge(userStateController.age + 1);
-        },
-        tooltip: 'Increment Age',
-        child: const Icon(Icons.add),
       ),
     );
   }
